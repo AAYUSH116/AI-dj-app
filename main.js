00a310b572aa603f31ed548f9d3689f3a7db054b
AAYUSH116 <82816625+AAYUSH116@users.noreply.github.com>
2021-05-28 17:41:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+sound="";
+
+function preLoad(){
+    sound=loadSound("music.mp3");
+}
+function setup(){
+    canvas=createCanvas(600,600);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video,0,0,600,600);
+}
+
+function play_song(){
+    sound.play_song();
+}
